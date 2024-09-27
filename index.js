2ddb9dc7d66661df505ab8affce11b0278095649
CLSnapp <repbandito@hotmail.com>
2024-09-26 20:35:14 -0500

    initalize

---
+++ index.js
@@ -0,0 +1,34 @@
+//Pseudocode:
+//1. Assign three variables. Each variable contains the corresponding result of calculation using a unique arithmetic operator. Each equal to one of the three codes in the combination.
+//2. Create a dialog box displaying the vault codes and the text in a popup dialog.
+//3. Uploaded secure the vault to GitHub.
+
+let sum = 0;
+let newNumber = 10;
+sum = sum + newNumber;
+console.log(sum);
+//expect output: The sum is equal to 10, giving you the first variable to the combination.
+
+let sum2 = 10;
+let newNumber2 = 4;
+sum2 *= newNumber2;
+console.log(sum2);
+//expected output: Sum2 is multiplied by newNumber2, giving the second variable to the combination.
+
+let sum3 = 40;
+let newNumber3 = 1;
+sum3--;
+console.log(sum3);
+//expected output: Sum3 is subtracted by newNumber3, giving you the third and final variable to the combination.
+
+const dialog =
+  "have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+let number1 = "10";
+let number2 = "40";
+let number3 = "39";
+
+
+//expected output: Alert popup in browser, showing the dialog + the number combinations.
+
+alert(`You  ${dialog}  ${number1}  -  ${number2}  -  ${number3}`)
+//expected output: message stating the secret combination
